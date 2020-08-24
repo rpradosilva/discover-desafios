69ddbd84a2d782a7225317c170b63c25b911c286
rpradosilva <contato@rprado.design>
2020-08-24 12:06:19 -0300

    Fix: challenge 1-1

--- src/challenge_1-1/index.js
+++ src/challenge_1-1/index.js
@@ -1,9 +1,13 @@
 var endereco = {
     rua: "Rua dos pinheiros",
     numero: 1293,
-    bairro: "Jabaquara",
+    bairro: "Centro",
     cidade: "São Paulo",
     uf: "SP"
 }
 
-console.log(`O usuário mora em ${endereco.cidade} / ${endereco.uf}, no bairro ${endereco.bairro}, na rua ${endereco.rua} com nº ${endereco.numero}.`)
+function printData(userData) {
+    console.log(`O usuário mora em ${userData.cidade} / ${userData.uf}, no bairro ${userData.bairro}, na rua ${userData.rua} com nº ${userData.numero}.`)
+}
+
+printData(endereco)
